b206c7fb1364d6c95bbdf0c971b543e2efc4deca
scarlet <sarahscarlet641@gmail.com>
2024-05-29 11:46:41 +0300

    complete

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        if isinstance(page_count, int):
+            self.page_count = page_count
+        else:
+            raise ValueError("page_count must be an integer")
+
+    def __str__(self):
+        return f"{self.title} ({self.page_count} pages)"
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+def test_requires_int_page_count(self):
+    book = Book("And Then There Were None", 272)
+    with self.assertRaises(ValueError):
+        book.page_count = "not an integer"
+
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        if isinstance(size, int):
+            self.size = size
+        else:
+            raise ValueError("size must be an integer")
+        self.condition = "New"
+
+    def __str__(self):
+        return f"{self.brand} shoe, size {self.size}"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+def test_requires_int_size(self):
+    stan_smith = Shoe("Adidas", 9)
+    with self.assertRaises(ValueError):
+        stan_smith.size = "not an integer"
+    
